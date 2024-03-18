a43067c6b4bf53eb9f46fc0c35d7817f71f16281
felipe <felipepassada@gmail.com>
2024-03-17 23:06:59 -0300

    feat: add email validation

--- src/main/java/com/felipe/passada/domain/entities/Email.java
+++ src/main/java/com/felipe/passada/domain/entities/Email.java
@@ -9,4 +9,27 @@ public class Email {
     @Getter
     String address;
 
+    public Email(String address) {
+        if(!isValidEmail(address)) {
+            throw new IllegalArgumentException("Invalid Email adress!!");
+        }
+
+        this.address = address;
+    }
+
+    public static boolean isValidEmail(String email) {
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+
+        int atIndex = email.indexOf('@');
+        if (atIndex == -1 || atIndex == 0 || atIndex == email.length() - 1) {
+            return false;
+        }
+
+        // Check if there is any character afeter the @
+        String beforeAt = email.substring(0, atIndex);
+        String afterAt = email.substring(atIndex + 1);
+        return !beforeAt.isEmpty() && !afterAt.isEmpty();
+    }
 }
