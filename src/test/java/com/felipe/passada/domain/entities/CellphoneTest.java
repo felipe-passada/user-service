cf3b8eb6c8fba6bf2d507641ba89e3ca83322e99
felipe <felipepassada@gmail.com>
2024-03-17 23:09:49 -0300

    test: add unit tests for cellphone validations

--- src/test/java/com/felipe/passada/domain/entities/CellphoneTest.java
+++ src/test/java/com/felipe/passada/domain/entities/CellphoneTest.java
@@ -1,6 +1,23 @@
 package com.felipe.passada.domain.entities;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CellphoneTest {
-  
+    @Test
+    void ShouldThrowWhenInvalidPhone() {
+        assertThrows(IllegalArgumentException.class, () -> new Cellphone("011", "8797"));
+        assertThrows(IllegalArgumentException.class, () -> new Cellphone(null, null));
+        assertThrows(IllegalArgumentException.class, () -> new Cellphone("", ""));
+    }
+
+    @Test
+    void ShouldPassWhenValidEmail() {
+        Cellphone phone = new Cellphone("11", "901982350");
+
+        assertEquals("11", phone.getDdd());
+        assertEquals("901982350", phone.getCellNumber());
+    }
+
 }
