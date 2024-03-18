affe2dc54edf37261429090025d247c8b4f4c34d
felipe <felipepassada@gmail.com>
2024-03-17 23:09:07 -0300

    feat: add cellphone validations

--- src/main/java/com/felipe/passada/domain/entities/Cellphone.java
+++ src/main/java/com/felipe/passada/domain/entities/Cellphone.java
@@ -11,4 +11,22 @@ public class Cellphone {
     String ddd;
     String cellNumber;
 
+    public Cellphone(String ddd, String cellNumber) {
+        if(!validatePhoneNumber(ddd + cellNumber)) {
+           throw new IllegalArgumentException("Invalid cellphone number!!");
+        }
+        this.ddd = ddd;
+        this.cellNumber = cellNumber;
+    }
+
+    public static boolean validatePhoneNumber(String phoneNumber) {
+        String digitsOnly = phoneNumber.replaceAll("[^0-9]", "");
+
+        if (!phoneNumber.equals(digitsOnly)) {
+            return false;
+        }
+
+        int length = digitsOnly.length();
+        return (length >= 10 && length <= 11);
+    }
 }
